08050f25bf20cf626b290403debe30152d2b446e
lazkopat3522 <ikramamzkg@gmail.com>
2025-03-26 18:11:23 -0400

    triangle

--- array.java
+++ array.java
@@ -1,18 +1,30 @@
 
+import java.util.Random;
 import java.util.Scanner;
 
 public class array {
     public static void main(String[] args) {
-
+        Random rand = new Random();
+        int num=rand.nextInt(100);
         Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        for(int i = 1; i <= n; i++) {
-            for(int j = 1; j <= i; j++)
-            {
-                System.out.print("* ");
+        System.out.println("Guessed number = "+num);
+        int lottery=-20;
+        while(num!=lottery)
+        {
+            lottery=sc.nextInt();
+            if(num==lottery) {
+                System.out.println("You winn");
+                System.out.println("Bye bye");}
+            if(num<lottery) {
+                System.out.println("Your number is greate");
+            } else if(num>lottery) {
+                System.out.println("Your number is less");
             }
-            System.out.println();
         }
+
+
+
+
 
 
 
