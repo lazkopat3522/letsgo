8cf0901c489b022d76e62b4b96e2b531146c7bbe
lazkopat3522 <ikramamzkg@gmail.com>
2025-03-27 20:56:18 -0400

    multiplication table

--- multi_table.java
+++ multi_table.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class multi_table {
+public class multi_table { // multiplication table
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
